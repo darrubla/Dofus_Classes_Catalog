1ecebcd8de30303b38f3cc1769ae0cd54d9f140a
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-11 17:13:28 -0500

    Updates

---
+++ precache-manifest.985ff99060db767dcdaca8a05b2d5eae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659183f0cdd39d9ed7f622962772fda0",
+    "url": "/Dofus_Classes_Catalog/index.html"
+  },
+  {
+    "revision": "2735732508c93bc36971",
+    "url": "/Dofus_Classes_Catalog/static/css/main.40e4fc91.chunk.css"
+  },
+  {
+    "revision": "a4970f82964f679c14cb",
+    "url": "/Dofus_Classes_Catalog/static/js/2.12cf6d9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dofus_Classes_Catalog/static/js/2.12cf6d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2735732508c93bc36971",
+    "url": "/Dofus_Classes_Catalog/static/js/main.17cb517d.chunk.js"
+  },
+  {
+    "revision": "370948a1c2137525f6a9",
+    "url": "/Dofus_Classes_Catalog/static/js/runtime-main.61d98405.js"
+  },
+  {
+    "revision": "4a0691836ae2d8e51d43a4a97953e99b",
+    "url": "/Dofus_Classes_Catalog/static/media/dofus.4a069183.png"
+  }
+]);
